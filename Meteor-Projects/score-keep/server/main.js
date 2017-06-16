0c76cbfcf4a45c20eb9bcf6f27013a3ed12b6772
shashank7200 <shashank7200@gmail.com>
2017-06-17 00:11:26 +0530

    removed Tracker autorun function

--- Meteor-Projects/score-keep/server/main.js
+++ Meteor-Projects/score-keep/server/main.js
@@ -6,14 +6,4 @@ import { Players } from './../imports/api/players'
 
 
 Meteor.startup(function(){
-  Players.insert({
-    name : "Shashank",
-    score: 3
-  });
-  Players.insert({
-    name : "Ehtesham",
-    score: 55
-  });
-
-console.log(Players.find().fetch());
 });
