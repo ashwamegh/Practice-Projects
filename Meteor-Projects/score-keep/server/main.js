a3344543907c83d08f9c48d897d3bf206b6f6571
shashank7200 <shashank7200@gmail.com>
2017-06-16 19:46:08 +0530

    seedes with initial experimental code

--- Meteor-Projects/score-keep/server/main.js
+++ Meteor-Projects/score-keep/server/main.js
@@ -1,1 +1,19 @@
-console.log("Log from /server/main.js");
+/*  Global Imports  */
+import { Meteor } from 'meteor/meteor'
+
+/*  Local Imports  */
+import { Players } from './../imports/api/players'
+
+
+Meteor.startup(function(){
+  Players.insert({
+    name : "Shashank",
+    score: 3
+  });
+  Players.insert({
+    name : "Ehtesham",
+    score: 55
+  });
+
+console.log(Players.find().fetch());
+});
