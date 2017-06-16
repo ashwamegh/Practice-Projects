5fe230e30807d412576dc7b531ba859d0f51b585
shashank7200 <shashank7200@gmail.com>
2017-06-17 00:10:23 +0530

    added players documents to be rendered on screen

--- Meteor-Projects/score-keep/client/main.js
+++ Meteor-Projects/score-keep/client/main.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
+import {Players} from './../imports/api/players';
 
 import App from './../imports/ui/App';
 
 Meteor.startup(() =>{
-  ReactDOM.render(<App />, document.getElementById('app'));
+  Tracker.autorun(function(){
+    let players = Players.find({}).fetch();
+
+    let jsx = (
+      <div>
+        <App />
+        { players.map((player) => <p key={player._id}>{player.name} have scored {player.score} point(s).</p>) }
+      </div>
+    );
+
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
+
+  let playerName = prompt("Enter the Player name");
+  let playerPoints = prompt("Enter the player score.");
+  playerName!==null && playerPoints!==null? Players.insert({name: playerName, score: playerPoints}):alert("Both fields are required");
+  console.log(Players.find({name: playerName}).fetch());
+
 });
