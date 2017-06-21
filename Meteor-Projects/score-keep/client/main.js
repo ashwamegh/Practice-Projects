55c6277042f8c80a39f6c283c3921067f02c0ae7
shashank7200 <shashank7200@gmail.com>
2017-06-21 17:20:09 +0530

    added increment, decrement score and remove player buttons

--- Meteor-Projects/score-keep/client/main.js
+++ Meteor-Projects/score-keep/client/main.js
@@ -32,7 +32,15 @@ Meteor.startup(() =>{
           <input type="text" name="player"/>
           <button type="submit">Add Player</button>
         </form>
-        { players.map((player) => <p key={player._id}>{player.name} have scored {player.score} point(s).</p>) }
+        { players.map((player) => 
+          <p key={player._id}>
+          {player.name} have scored {player.score} point(s).
+          <button onClick={ () => player.score===0
+          ?alert("You are at your lowest Score, Shame on You!")
+          :Players.update(player._id, {$inc: { score : -1}}) }>-1</button>
+          <button onClick={ () => Players.update(player._id, {$inc: { score : 1}}) }>+1</button>
+          <button onClick={ () => Players.remove(player._id) }>X</button>
+          </p>) }
       </div>
     );
 
