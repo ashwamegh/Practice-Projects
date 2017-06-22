c7af1ce61e698aba5f406be446456cfaadc8d7fb
shashank7200 <shashank7200@gmail.com>
2017-06-23 00:45:55 +0530

    added port 3001 as default port

--- Node-Projects/authentication-with-express-and-mongo/app.js
+++ Node-Projects/authentication-with-express-and-mongo/app.js
@@ -35,6 +35,6 @@ app.use(function(err, req, res, next) {
 });
 
 // listen on port 3000
-app.listen(3000, function () {
-  console.log('Express app listening on port 3000');
+app.listen(3001, function () {
+  console.log('Express app listening on port 3001');
 });
