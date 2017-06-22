8ee5f353a362f4881112d02852321f05be521e0a
shashank7200 <shashank7200@gmail.com>
2017-06-23 00:18:52 +0530

    project initial starter files

--- Node-Projects/authentication-with-express-and-mongo/app.js
+++ Node-Projects/authentication-with-express-and-mongo/app.js
@@ -1,31 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
 var app = express();
-
-// mongodb connection
-mongoose.connect("mongodb://localhost:27017/bookworm");
-var db = mongoose.connection;
-// mongo error
-db.on('error', console.error.bind(console, 'connection error:'));
-
-// use sessions for tracking logins
-app.use(session({
-  secret: 'treehouse loves you',
-  resave: true,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongooseConnection: db
-  })
-}));
-
-// make user ID available in templates
-app.use(function (req, res, next) {
-  res.locals.currentUser = req.session.userId;
-  next();
-});
 
 // parse incoming requests
 app.use(bodyParser.json());
