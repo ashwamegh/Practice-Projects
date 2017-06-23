482cebab33a71a6ac903a1d9a1d99f3b8d0fc585
shashank7200 <shashank7200@gmail.com>
2017-06-23 15:28:27 +0530

    added UserSchema and data checks

--- authentication-with-express-and-mongo/routes/index.js
+++ authentication-with-express-and-mongo/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// Importing Mongoose Schema
+let User = require('../models/user');
+
 // GET /
 router.get('/', function(req, res, next) {
   return res.render('index', { title: 'Home' });
@@ -24,7 +27,35 @@ router.get('/register', (req, res, next) => {
 
 // POST /register
 router.post('/register', (req, res, next) => {
-  return res.send('User Created');
+  if( req.body.name && req.body.favoriteBook && req.body.password && req.body.confirmPassword && req.body.email){
+
+    if( req.body.password !== req.body.confirmPassword){
+      let err = new Error(" Passwords do not match.");
+      err.status = 400;
+      return next(err);
+    }
+
+    //  creating User object
+    let userData = {
+      email : req.body.email,
+      name : req.body.name,
+      favoriteBook: req.body.favoriteBook,
+      password : req.body.password
+    };
+
+    User.create(userData, (error , user)=>{
+      if (error){
+        return next(error);
+      }else {
+        return res.redirect('/profile');
+      }
+    })
+
+  }else {
+    let err = new Error("All Fields are required!");
+    err.status = 400;
+    return next(err);
+  }
 });
 
 
