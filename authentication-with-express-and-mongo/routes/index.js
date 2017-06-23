4b9a5144f1ff15d7e3b96589506291d70ee2c3d1
shashank7200 <shashank7200@gmail.com>
2017-06-23 13:35:52 +0530

    added register route

--- authentication-with-express-and-mongo/routes/index.js
+++ authentication-with-express-and-mongo/routes/index.js
@@ -16,4 +16,16 @@ router.get('/contact', function(req, res, next) {
   return res.render('contact', { title: 'Contact' });
 });
 
+// GET /register
+router.get('/register', (req, res, next) => {
+  return res.render('register', { title: 'Sign Up'});
+});
+
+
+// POST /register
+router.post('/register', (req, res, next) => {
+  return res.send('User Created');
+});
+
+
 module.exports = router;
