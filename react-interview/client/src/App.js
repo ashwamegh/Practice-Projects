04b3f1bfc615108721c0b7ab98dfe051a4473d1b
shashank7200 <shashank7200@gmail.com>
2017-07-01 17:34:27 +0530

    initial commit

--- react-interview/client/src/App.js
+++ react-interview/client/src/App.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {users: []}
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
-
   render() {
     return (
       <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
--- react-interview/routes/users.js
+++ react-interview/routes/users.js
@@ -4,17 +4,18 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	// Comment out this line:
-  //res.send('respond with a resource');
+  console.log("Portpt");
+  res.send('respond with a resource');
 
   // And insert something like this instead:
-  res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }]);
-  console.log("We are in users URI.");
+  // res.json([{
+  // 	id: 1,
+  // 	username: "samsepi0l"
+  // }, {
+  // 	id: 2,
+  // 	username: "D0loresH4ze"
+  // }]);
+  // console.log("We are in users URI.");
 });
 
 module.exports = router;
