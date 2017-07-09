bd8d6842600904001c668cf1e6ebdbddde62e322
shashank7200 <shashank7200@gmail.com>
2017-07-09 15:40:04 +0530

    handling inputs from search bar

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 
 
-class SearchBar extends Component{
-    constructor(props){
-        super(props);
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            term:''
-        }
+    this.state = {
+      term: ''
     }
+  }
 
-    render(){
-        return (
-          <div>
-            <input value={ this.state.term } onChange={ (event) => this.setState({ term: event.target.value }) }/>
-          </div>
-        );
-    }
+  onInputChange(term) {
+    const { onSearchTermChange } = this.props;
+    
+    this.setState({ term });
+    onSearchTermChange(term);
+  }
+
+  render() {
+    const { onSearchTermChange } = this.props;
+
+    return (
+      <div className="search-bar">
+        <input value={this.state.term} onChange={ event => this.onInputChange(event.target.value) } />
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
