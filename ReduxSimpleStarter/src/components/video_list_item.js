a85c315d77691a0ce7c5bf18289a73efd6c96b2e
shashank7200 <shashank7200@gmail.com>
2017-07-09 15:40:33 +0530

    added function to be called on video selection

--- ReduxSimpleStarter/src/components/video_list_item.js
+++ ReduxSimpleStarter/src/components/video_list_item.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 
 const VideoListItem = (props) => {
-  const { video } = props,
+  const { video, onVideoSelect  } = props,
         imageUrl = video.snippet.thumbnails.default.url,
         heading = video.snippet.title;
 
   return (
-    <li className="list-group-item">
+    <li  onClick={ () => onVideoSelect(video) } className="list-group-item">
       <div className="video-list media">
 
         <div className="media-left">
