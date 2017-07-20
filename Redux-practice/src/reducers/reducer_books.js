0526318738ca41f0a3d9c9012beedf245f2ea63e
shashank7200 <shashank7200@gmail.com>
2017-07-20 17:54:43 +0530

    minor additions

--- Redux-practice/src/reducers/reducer_books.js
+++ Redux-practice/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
   return [
-    {title: 'Javascript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'Eloquent Ruby'},
-    {title: 'Half Girlfriend'}
+    {title: 'Javascript: The Good Parts', pages: 101},
+    {title: 'Harry Potter', pages: 459},
+    {title: 'Eloquent Ruby', pages:1},
+    {title: 'Half Girlfriend', pages:172}
   ];
 }
