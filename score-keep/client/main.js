7beee0c6a462a18af8306e25d6bf7298800d25a2
shashank7200 <shashank7200@gmail.com>
2017-07-30 16:12:23 +0530

    refractored components

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -3,52 +3,19 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
 import {Players} from './../imports/api/players';
-import TitleBar from './../imports/ui/TitleBar';
-import AddPlayer from './../imports/ui/AddPlayer';
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  let playerName = event.target.player.value;
-
-  if (playerName) {
-    event.target.player.value = '';
-
-    Players.insert({name: playerName, score: 0});
-
-  }
-};
+import App from './../imports/ui/App';
 
 Meteor.startup(() => {
   Tracker.autorun(function() {
-    let players = Players.find({}).fetch();
+    let players = Players.find({},{ sort: { score: -1} }).fetch();
+    let title = "Score Keep";
+    let subTitle = "Created by Shashank Shekhar";
 
-    let jsx = (
-      <div>
-        <TitleBar />
-          {players.map((player) => <p key={player._id}>
-            {player.name} have scored {player.score} point(s).
-            <button onClick={() => player.score === 0
-              ? alert("You are at your lowest Score, Shame on You!")
-              : Players.update(player._id, {
-                $inc: {
-                  score: -1
-                }
-              })}>-1</button>
-            <button onClick={() => Players.update(player._id, {
-              $inc: {
-                score: 1
-              }
-            })}>+1</button>
-            <button onClick={() => Players.remove(player._id)}>X</button>
-          </p>)}
-
-        <AddPlayer onSubmit={handleSubmit}/>
-
-      </div>
-    );
-
-    ReactDOM.render(jsx, document.getElementById('app'));
+    ReactDOM.render(
+      <App
+        title={ title}
+        players={ players }
+        subTitle={ subTitle }/>, document.getElementById('app'));
   });
 
 });
