cb543cf33a4a75e0e96044775dfbd0bf4214cf6a
shashank7200 <shashank7200@gmail.com>
2017-07-30 16:14:34 +0530

    Added propTypes checking and props

--- score-keep/imports/ui/AddPlayer.jsx
+++ score-keep/imports/ui/AddPlayer.jsx
@@ -1,8 +1,22 @@
 import React, { Component} from 'react';
+import {Players} from './../api/players';
 
-export default AddPlayer = (props) => (
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  let playerName = event.target.player.value;
+
+  if (playerName) {
+    event.target.player.value = '';
+
+    Players.insert({name: playerName, score: 0});
+
+  }
+};
+
+export default AddPlayer = () => (
       <div>
-        <form onSubmit={props.onSubmit}>
+        <form onSubmit={ handleSubmit }>
           <input type="text" name="player"/>
           <button type="submit">Add Player</button>
         </form>
--- score-keep/imports/ui/TitleBar.jsx
+++ score-keep/imports/ui/TitleBar.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class TitleBar extends React.Component{
   render(){
     return(
-      <div><h1>My App 2</h1></div>
+      <div>
+        <h1>{ this.props.title }</h1>
+        {this.props.subTitle?<h2>{ this.props.subTitle }</h2>:undefined}
+      </div>
     );
   }
 }
+
+TitleBar.propTypes ={
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string
+};
 
 export default TitleBar;
