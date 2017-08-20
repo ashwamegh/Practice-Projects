1336553890e82b7ff82d5fc34431f4ffe052410b
shashank7200 <shashank7200@gmail.com>
2017-08-20 23:33:58 +0530

    minor changes

--- score-keep/imports/ui/TitleBar.jsx
+++ score-keep/imports/ui/TitleBar.jsx
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 class TitleBar extends React.Component{
   render(){
     return(
-      <div>
-        <h1>{ this.props.title }</h1>
-        {this.props.subTitle?<h2>{ this.props.subTitle }</h2>:undefined}
+      <div className="title-bar">
+        <div className="wrapper">
+          <h1>{ this.props.title }</h1>
+          {this.props.subTitle?<h2>{ this.props.subTitle }</h2>:undefined}
+        </div>
       </div>
     );
   }
