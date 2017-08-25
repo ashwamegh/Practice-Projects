20d209358e56f71666d57afe84a0d53a263e1a57
shashank7200 <shashank7200@gmail.com>
2017-08-25 20:06:48 +0530

    minor correction in link

--- you-react-pwa/src/components/VideoList.jsx
+++ you-react-pwa/src/components/VideoList.jsx
@@ -9,7 +9,7 @@ const VideoList = props => {
     <div className="col-md-12 list-group">
       {videos.map(video =>
         <a
-          href={`https://www.youtube.com/watch?v${video.id}`}
+          href={`https://www.youtube.com/watch?v=${video.id}`}
           key={video.etag}
           target="_blank"
           rel="noopener"
