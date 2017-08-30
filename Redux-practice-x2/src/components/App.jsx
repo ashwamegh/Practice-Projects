dd12b677c08a64f2b67e9cc1a215dee6815804be
shashank7200 <shashank7200@gmail.com>
2017-08-30 21:15:22 +0530

    Redux Booklist app final wrapup

---
+++ Redux-practice-x2/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import BookList from './../containers/BookList';
+import BookDetail from './../containers/BookDetail';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      React simple starter
+      <BookList />
+      <BookDetail />
+      </div>
+    );
+  }
+}
