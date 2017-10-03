8dc6321f7d30727c5cf4df1738caeb9e5c30cf24
Shashank Shekhar <shashank7200@gmail.com>
2017-10-03 23:59:56 +0530

    Using 3rd Party Modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,11 +1,23 @@
-console.log("Starting App");
+console.log("Starting app.js");
 
+// Global modules
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+
+// Local Modules
 const user = os.userInfo();
+const notes = require('./notes.js');
 
 
-fs.appendFile('messages.txt', `Hello ${user.username}!`, (err) =>{
-  if(err) throw err;
-  console.log("data added");
-});
+console.log(_.isString(true));
+console.log(_.isString("true"));
+// fs.appendFile('messages.txt', `Hello ${user.username}!`, (err) =>{
+//   if(err) throw err;
+//   console.log("data added");
+// });
+
+const result = notes.add(212,32);
+
+
+console.log(result);
---
+++ notes-node/notes.js
@@ -0,0 +1,7 @@
+console.log("Starting notes.js");
+
+module.exports = {
+  add: (a,b) => {
+    return a+b;
+  }
+}
