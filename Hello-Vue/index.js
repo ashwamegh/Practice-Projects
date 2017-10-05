746727d98ff9637fec5b6555d869b0d5b7ceb64c
Shashank Shekhar <shashank7200@gmail.com>
2017-10-06 00:03:37 +0530

    added vue event listeners

--- Hello-Vue/index.js
+++ Hello-Vue/index.js
@@ -1,6 +1,23 @@
-new Vue({
+let app = new Vue({
   el:'#root',
   data: {
-    message:"Hello World"
+    message:"Hello World",
+    tagline:"Vue is Sexy",
+    nameArray: ['John', 'Sita', 'Geeta', 'Sunita'],
+    nameInput:'',
+  },
+
+  methods:{
+  	addName(){
+  		this.nameArray.push(this.nameInput);
+  		this.nameInput = '';
+  	}
   }
-})
+//   mounted (){
+//   	document.getElementById('addName').addEventListener('click', () => {
+// 	const name = document.getElementById('nameInput');
+// 	app.nameArray.push(name.value);
+// 	name.value = "";
+// });
+//   }
+});
