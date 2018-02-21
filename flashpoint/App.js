be0ed37b5a0742e18b16d628ece33ba48cf01738
Shashank Shekhar <shashank7200@gmail.com>
2018-02-21 10:44:26 +0530

    made few changes

--- flashpoint/App.js
+++ flashpoint/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Platform, Button, NativeModules } from "react-native";
+import { Torch } from NativeModules;
 
 export default class App extends React.Component {
+  torchHandler() {
+    // if (Platform.OS === "ios") {
+    //   Torch.switchState(this.isTorchOn);
+    // } else {
+    //   const cameraAllowed = await Torch.requestCameraPermission(
+    //     'Camera Permissions', // dialog title
+    //     'We require camera permissions to use the torch on the back of your phone.' // dialog body
+    //   );
+
+    //   if (cameraAllowed) {
+    //     Torch.switchState(this.isTorchOn);
+    //   }
+    // }
+    Torch.setTorch(true); // Turn ON
+// Torch.switchState(false);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          onPress={this.torchHandler}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
       </View>
     );
   }
@@ -16,8 +40,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
