cf51a2383791b7bd334f29ad053c12a8db8fca19
Shashank Shekhar <shashank7200@gmail.com>
2018-03-03 11:57:31 +0530

    compiled js for all the riot tags

--- riotjs-demo/tags/all.js
+++ riotjs-demo/tags/all.js
@@ -1,10 +1,10 @@
-riot.tag2('hello-form', '<form onsubmit="{sayHello}"> <input type="text" ref="greet" name="greet"> <button type="submit">Say Hello</button> </form> <h4>Hello {this.greeting} </h4>', '', '', function(opts) {
- this.sayHello = (event) => {
+riot.tag2('hello-form', '<form onsubmit="{sayHello}"> <input type="text" ref="greet" name="greet"> <button type="submit">Say Hello</button> </form> <h5>Hello {this.greeting} </h5>', '', '', function(opts) {
+
+ this.sayHello = function (event) {
     event.preventDefault();
     this.greeting = this.refs.greet.value;
     document.querySelector('input[name="greet"]').value = '';
-  }
-
+}.bind(this)
 });
 riot.tag2('hello-world', '<h1>Hello {opts.greet}</h1> <h3>Its Sat, 3rd March Today</h3>', '', '', function(opts) {
 });
