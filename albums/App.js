b4502c5e4843044b057e4c0c16fa1f9d6f357f2c
Shashank Shekhar <shashank7200@gmail.com>
2018-06-29 01:11:45 +0530

    created a header component

--- albums/App.js
+++ albums/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './src/components/header';
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Header />
       </View>
     );
   }
---
+++ albums/src/components/header.js
@@ -0,0 +1,9 @@
+//  Imports 
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => (
+    <Text>Albums</Text>
+  );
+
+export default Header;
