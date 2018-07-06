baf47082538a4b59e9e4515fa0c1332ac72042d1
Shashank Shekhar <shashank7200@gmail.com>
2018-07-07 01:02:03 +0530

    added state of the component and making a album list using api data

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,18 +1,41 @@
 //  Imports
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import axios from 'axios';
 
 class AlbumList extends Component {
 
+  state = {
+    albums: []
+  }
+
+  /**
+   * Lifecylce method, when the component will be mounted
+   */
   componentWillMount() {
-    
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response) => {
+      this.setState({ albums: response.data });
+    });
+  }
+
+  /**
+   * Method to render the albums list using simple album list
+   */
+  renderAlbums() {
+    const { albums } = this.state;
+
+    return albums.map((album) => <Text>{album.title}</Text>);
   }
   
+
+  /**
+   * Renders UI for this component
+   */
   render() {
     return (
       <View>
-      <Text>Album List!!!!</Text>
+      { this.renderAlbums() }
     </View>
     );
   }
