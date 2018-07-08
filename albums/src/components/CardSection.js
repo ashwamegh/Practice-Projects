c65bd49f0beeba51b0583debcf26f938f45edac9
Shashank Shekhar <shashank7200@gmail.com>
2018-07-08 13:03:16 +0530

    minor typos fix

--- albums/src/components/CardSection.js
+++ albums/src/components/CardSection.js
@@ -12,7 +12,7 @@ const CardSection = (props) => (
 
 const styles = { 
   containerStyle: {
-    borderBotttomWidth: 1,
+    borderBottomWidth: 1,
     padding: 5,
     backgroundColor: '#fff',
     justifyContent: 'flex-start',
