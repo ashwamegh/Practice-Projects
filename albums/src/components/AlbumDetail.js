62a500b6285edd428fbdb8d2313e82daa5701e4e
Shashank Shekhar <shashank7200@gmail.com>
2018-07-08 12:37:36 +0530

    added Image under the cards section for the album detail

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,23 +1,38 @@
 // ===================  Imports ======================= //
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = props => {
   const { album } = props;
+  const { headerContentStyle, imageStyle } = styles;
 
   return (
     <Card>
       <CardSection>
-        <Text>{album.title}</Text>
-      </CardSection>
-      <CardSection>
-        <Text>{album.title}</Text>
+      <View>
+        <Image source={{ uri: album.thumbnail_image }} style={imageStyle} />
+      </View>
+        <View style={headerContentStyle}>
+          <Text>{album.title}</Text>
+          <Text>{album.artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  imageStyle: {
+    height: 50,
+    width: 50
+  }
 };
 
 export default AlbumDetail;
