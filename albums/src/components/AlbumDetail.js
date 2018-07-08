21a19177ea998b9a6d89b20ba69cc1f212950a85
Shashank Shekhar <shashank7200@gmail.com>
2018-07-08 13:03:47 +0530

    added text styles and a card section to show the image content

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -7,18 +7,22 @@ import CardSection from './CardSection';
 
 const AlbumDetail = props => {
   const { album } = props;
-  const { headerContentStyle, imageStyle } = styles;
+  const { headerContentStyle, thumbnailStyle, thumbnailContainerStyle, imageStyle, headerTextStyle } = styles;
 
   return (
     <Card>
       <CardSection>
-      <View>
-        <Image source={{ uri: album.thumbnail_image }} style={imageStyle} />
+      <View style={thumbnailContainerStyle}>
+        <Image source={{ uri: album.thumbnail_image }} style={thumbnailStyle} />
       </View>
         <View style={headerContentStyle}>
-          <Text>{album.title}</Text>
+          <Text style={headerTextStyle} >{album.title}</Text>
           <Text>{album.artist}</Text>
         </View>
+      </CardSection>
+
+      <CardSection>
+        <Image source={{ uri: album.image }} style={imageStyle} />
       </CardSection>
     </Card>
   );
@@ -29,9 +33,24 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
-  imageStyle: {
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
