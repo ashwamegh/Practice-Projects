f51ace7b53d1b15f4aecaa3e45f7c6c8e4087091
Shashank Shekhar <shashank7200@gmail.com>
2018-07-11 14:23:36 +0530

    added eslint and minor refractoring

--- auth-react-native/App.js
+++ auth-react-native/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Header } from "./src/components/common";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import firebase from 'firebase';
+import { Header } from './src/components/common';
 
 export default class App extends Component {
   componentWillMount() {
     const config = {
-      apiKey: "AIzaSyCcZmq_YvT0IYOc8b9aXjq1h_MWy-xm4l0",
-      authDomain: "auth-react-native-1a790.firebaseapp.com",
-      databaseURL: "https://auth-react-native-1a790.firebaseio.com",
-      projectId: "auth-react-native-1a790",
-      storageBucket: "auth-react-native-1a790.appspot.com",
-      messagingSenderId: "68918987705"
+      apiKey: 'AIzaSyCcZmq_YvT0IYOc8b9aXjq1h_MWy-xm4l0',
+      authDomain: 'auth-react-native-1a790.firebaseapp.com',
+      databaseURL: 'https://auth-react-native-1a790.firebaseio.com',
+      projectId: 'auth-react-native-1a790',
+      storageBucket: 'auth-react-native-1a790.appspot.com',
+      messagingSenderId: '68918987705',
     };
 
     firebase.initializeApp(config);
@@ -20,8 +20,10 @@ export default class App extends Component {
   render() {
     return (
       <View>
-        <Header headerText={"Authentication"} />
-        <Text>Open up App.js to start working on your app!</Text>
+        <Header headerText="Authentication" />
+        <Text>
+Open up App.js to start working on your app!
+        </Text>
       </View>
     );
   }
