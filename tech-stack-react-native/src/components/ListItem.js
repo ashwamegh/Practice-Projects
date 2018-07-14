bc5348f4612a769d7c534601995ff986b8c19761
Shashank Shekhar <shashank7200@gmail.com>
2018-07-14 14:09:06 +0530

    added logic to render the description text

--- tech-stack-react-native/src/components/ListItem.js
+++ tech-stack-react-native/src/components/ListItem.js
@@ -1,33 +1,56 @@
 // ===================  Imports ======================= //
 import React, { Component } from "react";
-import { Text } from "react-native";
-import { connect } from 'react-redux';
-import { CardSection } from './common';
-import * as actions from './../actions';
-
+import { View, Text, TouchableWithoutFeedback } from "react-native";
+import { connect } from "react-redux";
+import { CardSection } from "./common";
+import * as actions from "./../actions";
 
 // Component Declaration
 
-class ListIten extends Component{
-  render(){
-    const  { library } = this.props;
+class ListIten extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.renderDescription = this.renderDescription.bind(this);
+  }
+
+  renderDescription(){
+    const { library, selectedLibraryId } = this.props;
+    if(library.id === selectedLibraryId){
+      return (
+        <Text>
+          { library.description }
+        </Text>
+      )
+    }
+  }
+
+  render() {
+    const { library, selectLibrary } = this.props;
     const { titleStyle } = styles;
 
     return (
-      <CardSection>
-        <Text style={titleStyle}>
-          { library.title }
-        </Text>
-      </CardSection>
-    )
+      <TouchableWithoutFeedback onPress={ () => selectLibrary(library.id)}>
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>{library.title}</Text>
+          </CardSection>
+        { this.renderDescription() }
+        </View>
+      </TouchableWithoutFeedback>
+    );
   }
 }
 
 const styles = {
-  titleStyle:{
+  titleStyle: {
     fontSize: 18,
     paddingLeft: 15
   }
-}
+};
 
-export default connect(null, actions)(ListIten);
+export default connect(
+  null,
+  actions
+)(ListIten);
