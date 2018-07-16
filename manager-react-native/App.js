5d4f997da5705d9599f070fd0fbddc3886bdb3ae
Shashank Shekhar <shashank7200@gmail.com>
2018-07-16 12:16:19 +0530

    added Header and Login form in the app component

--- manager-react-native/App.js
+++ manager-react-native/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import firebase from 'firebase';
+import firebase from "firebase";
 
-import reducers from './src/reducers';
+import reducers from "./src/reducers";
+import LoginForm from "./src/components/LoginForm";
+import { Header } from "./src/components/common";
 
 export default class App extends Component {
-  
-  componentWillMount(){
-    const  config = {
+  componentWillMount() {
+    const config = {
       apiKey: "AIzaSyBQ96RqE44YeMkSL8kHrjP_ynK8eh4E8JQ",
       authDomain: "manager-react-native-78193.firebaseapp.com",
       databaseURL: "https://manager-react-native-78193.firebaseio.com",
@@ -23,8 +24,9 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View style={styles.container}>
-          <Text>Hello</Text>
+        <View>
+          <Header headerText={"Manager"} />
+          <LoginForm />
         </View>
       </Provider>
     );
