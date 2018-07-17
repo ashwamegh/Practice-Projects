c0a8e68a4e42e4e17b314a7e84ad80dd7c9ee7a8
Shashank Shekhar <shashank7200@gmail.com>
2018-07-17 18:32:47 +0530

    added new action creators and thunks for fetching the firebase data on login

--- manager-react-native/src/actions/index.js
+++ manager-react-native/src/actions/index.js
@@ -1,9 +1,21 @@
+import * as firebase from 'firebase';
+import { EMAIL_CHANGED, PASSWORD_CHANGED } from "./types";
 
-import  { EMAIL_CHANGED } from './types';
-
-export const emailChanged = (email) => {
-  return{
+export const emailChanged = email => {
+  return {
     type: EMAIL_CHANGED,
     payload: email
-  }
+  };
+};
+
+export const passwordChanged = password => {
+  return {
+    type: PASSWORD_CHANGED,
+    password
+  };
+};
+
+export const loginUser = ({ email, password}) => (dispatch) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then(user => console.log(user));
 }
--- manager-react-native/src/reducers/AuthReducer.js
+++ manager-react-native/src/reducers/AuthReducer.js
@@ -1,9 +1,11 @@
-import { EMAIL_CHANGED } from './../actions/types';
+import { EMAIL_CHANGED,PASSWORD_CHANGED } from './../actions/types';
 
 export default (state={ email: ''}, action) => {
   switch(action.type){
     case EMAIL_CHANGED:
       return Object.assign({}, state, { email: action.email});
+    case PASSWORD_CHANGED:
+      return Object.assign({}, state, { password: action.password})
     default:
       return state;
   }
