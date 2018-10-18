455c606ca9e91cd7c7193e1bc3f4e1ce3ad0c6db
Shashank Shekhar <shashank7200@gmail.com>
2018-10-18 17:47:01 +0530

    created components

--- hello-next/pages/index.js
+++ hello-next/pages/index.js
@@ -1,5 +1,11 @@
+// imports
+import Link from 'next/link';
+import Header from '../components/Header';
+
+
 const Index = () => (
   <div>
+    <Header></Header>
     <p>Hello Next.js</p>
   </div>
 )
